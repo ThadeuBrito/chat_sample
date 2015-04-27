8a4282d93d42286e5c98e940c59963179f591d2b
Thadeu Brito <itvbrito@gmail.com>
2015-04-27 00:11:00 -0300

    Added socket.io

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 var express  = require('express');
 var http     = require('http');
+var socketio = require('socket.io');
 
 var app      = express();
 var server   = http.Server(app);
+var io       = socketio(server);
 
-server.listen(3000, function() {
-  console.log('listening on localhost:3000');
-});
+app.use('/', express.static(__dirname + '/'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+});
+
+server.listen(3000, function() {
+  console.log('listening on localhost:3000');
 });
