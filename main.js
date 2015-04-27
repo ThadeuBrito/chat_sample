661f28ef59c6c74c3b6596778765d317851b4a47
Thadeu Brito <itvbrito@gmail.com>
2015-04-27 00:28:33 -0300

    Set a event to the server using socket.io

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 var socket = io();
+
+$('.new-message-form').submit(function(){
+  socket.emit('chat message', $('#new-message-field').val());
+  $('#new-message-field').val('');
+  return false;
+});
